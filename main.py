3589e67db394054940496a6d2996e301e3ab0b75
iotquindio <iotquindio@gmail.com>
2023-11-13 13:18:41 -0500

    add modules folder

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hola proyecton Rapsberry Pi3')
+from email.message import EmailMessage
+import smtplib
+
+
+remitente = "iotquindio@gmail.com"
+destinatario = "camilogonzalezf8@gmail.com"
+
+mensaje = "Felicitaciones, si puede leer esto quiere decir que funcionó"
+
+email = EmailMessage()
+email["From"] = remitente
+email["To"] = destinatario
+email["Subject"] = "Mensaje de prueba Raspberry Pi3" 
+email.set_content(mensaje)
+smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+smtp.login(remitente, "pjbrzaijismmntpf")
+smtp.sendmail(remitente, destinatario, email.as_string())
+smtp.quit()
